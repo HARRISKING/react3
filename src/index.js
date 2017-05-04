4e42cac403064c551a74b4c05bf0dc2d3a7b0575
unknown <>
2017-05-04 12:28:39 +0800

    5.4

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <h1>我是第一个标签</h1>,
+  document.getElementById('root')
+);
+ReactDOM.render(
+  <h2>在react中插入两个便签，实验结果如何</h2>,
+  document.getElementById('root1')
+)
